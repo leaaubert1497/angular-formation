d7138482619e1c50f51d855617d9899987dfe3b4
Lea Aubert <lea.1.aubert@bnpparibas.com>
2023-03-21 13:46:43 +0100

    dev: added serve-index

--- back/server.js
+++ back/server.js
@@ -1,6 +1,8 @@
 console.log('About to start a server')
 
 const express = require('express')
+const serveIndex = require('serve-index')
+
 const app = express()
 const port = 3000
 
@@ -8,6 +10,11 @@ app.use((req, res, next) => {
     console.log('url: ' + req.path)
     next()
   })
+
+
+
+app.use(express.static('.'))
+app.use(serveIndex('.', { icons: true }))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
