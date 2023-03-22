2246d1e979e645f73d2c02c4bf67cc4bff240d21
Lea Aubert <lea.1.aubert@bnpparibas.com>
2023-03-22 16:27:20 +0100

    dev: article loop

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Article } from '../interfaces/article';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent {
-
+  articles: Article[] = [
+    {
+      id: 'a1',
+      name: 'pelle',
+      price: 3.99,
+      qty: 123,
+    },
+    {
+      id: 'a2',
+      name: 'rateau',
+      price: 8.99,
+      qty: 200,
+    },
+    {
+      id: 'a3',
+      name: 'marteau',
+      price: 10.0,
+      qty: 100,
+    },
+  ];
 }
