5b7992e4ef48fe6060ec1a6692a6122699adbdc2
Lea Aubert <lea.1.aubert@bnpparibas.com>
2023-03-23 15:41:11 +0100

    dev: added selected

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnDestroy } from '@angular/core';
 import { ArticleService } from '../services/article.service';
-import { faRotateRight, faPlus, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
-
+import {
+  faRotateRight,
+  faPlus,
+  faTrashAlt,
+} from '@fortawesome/free-solid-svg-icons';
+import { Article } from '../interfaces/article';
 
 @Component({
   selector: 'app-stock',
@@ -9,9 +13,10 @@ import { faRotateRight, faPlus, faTrashAlt } from '@fortawesome/free-solid-svg-i
   styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnDestroy {
-  faRotateRight = faRotateRight
-  faPlus = faPlus
-  faTrashAlt = faTrashAlt
+  faPlus = faPlus;
+  faRotateRight = faRotateRight;
+  faTrashAlt = faTrashAlt;
+  selectedArticles = new Set<Article>();
 
   constructor(protected readonly articleService: ArticleService) {
     console.log('articleService: ' + articleService);
@@ -19,5 +24,13 @@ export class StockComponent implements OnDestroy {
 
   ngOnDestroy(): void {
     console.log('onDestroy');
+  }
+
+  select(a: Article) {
+    if (this.selectedArticles.has(a)) {
+      this.selectedArticles.delete(a);
+      return;
+    }
+    this.selectedArticles.add(a);
   }
 }
